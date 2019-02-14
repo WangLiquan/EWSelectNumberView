ab82e09b4bb9c2dbbc7a760e149a08a318f54646
wangliquan <liquan.wang@croninfo.com>
2019-02-14 11:46:43 +0800

    更新实现frame自定制

--- EWSelectNumberView/EWSelectNumberView.swift
+++ EWSelectNumberView/EWSelectNumberView.swift
@@ -12,7 +12,7 @@ class EWSelectNumberView: UIView {
     /// 修改数量回调,减回调false,加回调true
     public var backEditNumber: ((Bool) -> ())?
     public var maxSelectNumber: Int = 99
-    ///FIXME: 更新添加上限
+    /// 选择数量,public,外部可调用
     public var selectedNumber: Int = 1{
         didSet{
             self.subtractButton.isEnabled = selectedNumber == 1 ? false : true
@@ -24,8 +24,8 @@ class EWSelectNumberView: UIView {
         let button = UIButton()
         button.isEnabled = false
         button.tag = 0
-        button.setImage(UIImage(named: "selectNumber_subtractIsEnableFalse"), for: .disabled)
-        button.setImage(UIImage(named: "selectNumber_subtractIsEnableTrue"), for: .normal)
+        button.setBackgroundImage(UIImage(named: "selectNumber_subtractIsEnableTrue"), for: .normal)
+        button.setBackgroundImage(UIImage(named: "selectNumber_subtractIsEnableFalse"), for: .disabled)
         return button
     }()
     private lazy var showLabel: UILabel = {
@@ -40,12 +40,13 @@ class EWSelectNumberView: UIView {
     private let addButton: UIButton = {
         let button = UIButton()
         button.tag = 1
-        button.setImage(UIImage(named: "selectNumber_addEnableTrue"), for: .normal)
-        button.setImage(UIImage(named: "selectNumber_addEnableFalse"), for: .disabled)
+        button.setBackgroundImage(UIImage(named: "selectNumber_addEnableTrue"), for: .normal)
+        button.setBackgroundImage(UIImage(named: "selectNumber_addEnableFalse"), for: .disabled)
         return button
     }()
 
     init(frame: CGRect, defaultNumber: Int = 1, maxNumber: Int = 99){
+        let frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: frame.width, height: frame.height < frame.width/3 ? frame.height : frame.width/3)
         super.init(frame: frame)
         self.maxSelectNumber = maxNumber
         self.setValue(defaultNumber, forUndefinedKey: "selectedNumber")
@@ -54,7 +55,7 @@ class EWSelectNumberView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    // 重写kvo方法才能完成赋值,不然报错
+    // 重写kvo方法才能完成赋值,不然不调用didSet方法
     override func setValue(_ value: Any?, forUndefinedKey key: String) {
         guard let selectedNumber = value as? Int else {
             return
@@ -63,14 +64,15 @@ class EWSelectNumberView: UIView {
     }
 
     private func drawMyView(){
+        let buttonWidth = self.frame.height
         self.addSubview(subtractButton)
         subtractButton.addTarget(self, action: #selector(onClickChangeButton(sender:)), for: .touchUpInside)
-        subtractButton.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        subtractButton.frame = CGRect(x: 0, y: 0, width: buttonWidth, height: buttonWidth)
         self.addSubview(addButton)
         addButton.addTarget(self, action: #selector(onClickChangeButton(sender:)), for: .touchUpInside)
-        addButton.frame = CGRect(x: 68, y: 0, width: 24, height: 24)
+        addButton.frame = CGRect(x: self.frame.width - buttonWidth, y: 0, width: buttonWidth, height: buttonWidth)
         self.addSubview(showLabel)
-        showLabel.frame = CGRect(x: 24, y: 0, width: 44, height: 24)
+        showLabel.frame = CGRect(x: buttonWidth, y: 0, width: self.frame.width - buttonWidth * 2, height: buttonWidth)
     }
     @objc private func onClickChangeButton(sender: UIButton) {
         switch sender.tag {
--- EWSelectNumberView/ViewController.swift
+++ EWSelectNumberView/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private let selectNumberView: EWSelectNumberView = EWSelectNumberView(frame: CGRect(x: 100, y: 300, width: 96, height: 24), defaultNumber: 99, maxNumber: 100)
+    private let selectNumberView: EWSelectNumberView = EWSelectNumberView(frame: CGRect(x: 100, y: 200, width: 96, height: 24), defaultNumber: 99, maxNumber: 100)
+
+    private let selectNumberView2: EWSelectNumberView = EWSelectNumberView(frame: CGRect(x: 89, y: 300, width: 150, height: 70), defaultNumber: 5, maxNumber: 100)
+
+    private let selectNumberView3: EWSelectNumberView = EWSelectNumberView(frame: CGRect(x: 96, y: 400, width: 200, height: 30), defaultNumber: 1, maxNumber: 10)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(selectNumberView)
+        self.view.addSubview(selectNumberView2)
+        self.view.addSubview(selectNumberView3)
     }
 
 
